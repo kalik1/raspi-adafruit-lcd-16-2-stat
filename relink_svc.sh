45ed376be988554583311c7a51ca190a41242cb6
kalik1 <milleniumbug89@hotmail.com>
2020-03-07 00:40:59 +0000

    first running

---
+++ relink_svc.sh
@@ -0,0 +1,1 @@
+npm unlink && systemctl stop lcd-status.service && systemctl disable lcd-status.service && sudo npm link &&  systemctl enable lcd-status.service  && systemctl start lcd-status.service
